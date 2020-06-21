1286be0d64c2fabc5bdbc24ace4c9a12ccbbd71f
Hewr Srood <hewr_s@outlook.com>
2020-06-21 23:10:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Your code here
 class Cat {
   constructor(name,sex) {
-    this.name=name;
-    this.sex=sex;
+    this.name=name
+    this.sex=sex
   }
   speak(){
   return `${this.name} says meow!`
